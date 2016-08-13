88f80dc411448a623c453cab62b44f03390d7726
slackfx <nandogimenes@gmail.com>
2016-08-13 19:50:16 -0300

    Initial Commit

---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Fernando Xavier on 13/08/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hypnosister/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hypnosister
+//
+//  Created by Fernando Xavier on 13/08/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
